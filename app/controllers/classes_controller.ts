c0a80429f0e743d7a89655cb9140ba99a4704c41
Israel R. <israel.rexc@yahoo.com>
2024-07-10 13:35:18 -0300

    feat: Delete class

--- app/controllers/classes_controller.ts
+++ app/controllers/classes_controller.ts
@@ -27,14 +27,28 @@ export default class ClassesController {
     // Validate
     const validated = await classUpdateValidator.validate(dt)
 
+    // Empty input
     if (myClass === null) {
       return response.abort({ message: 'Classe não encontrado' })
     }
+
+    // Save record
     myClass.classNo = request.input('class_no')
     myClass.capacity = request.input('capacity')
     myClass.available = request.input('available')
     myClass.save()
     return myClass
+  }
+
+  public async delete({ request, response }: HttpContext) {
+    const myClass: Class | null = await Class.find(request.param('id'))
+
+    // Empty del
+    if (myClass === null) {
+      return response.json({ message: 'Classe já não existe' })
+    }
+    myClass.delete()
+    return response.json({ message: 'Classe deletada', data: myClass })
   }
 }
 
